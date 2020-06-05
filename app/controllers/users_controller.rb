cabc7575ee216ce19c61dc5a6bceb33b2d6cdf00
Maclenn77 <maclenn77@gmail.com>
2020-06-05 15:45:31 -0500

    Add edit view. Add error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
   # require 'users_helper'
   # include UsersHelper
 
@@ -16,10 +17,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to new_user_path, notice: 'User account was successfully updated.' }
+        format.json { render :new, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+        flash.notice = "You can't use special characters with username. You need to provide a valid email"
+      end
+    end
+  end
+
+  def edit; end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
